2a4f84a8fd6de61948a4f9f46ec56168596da644
bellosprojects <bello.angel1505@gmail.com>
2025-12-23 12:57:06 -0400

    create script.js

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+@app.get("/api/status")
+async def read_status():
+    return {"status": "ok"}
+
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
